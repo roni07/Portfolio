3e1260ef78c259073b0da9627a1ea221216dd622
mehedi-seu <mdmehedihasanroni28@gmail.com>
2020-02-08 02:40:56 +0600

    added top and about section component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,11 +1,19 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppComponent } from './app.component';
+import {AppComponent} from './app.component';
+import {TopSectionComponent} from './main/top-section/top-section.component';
+import {FooterComponent} from './main/footer/footer.component';
+import {ResumeSectionComponent} from './main/resume-section/resume-section.component';
+import {AboutSectionComponent} from './main/about-section/about-section.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TopSectionComponent,
+    AboutSectionComponent,
+    ResumeSectionComponent,
+    FooterComponent,
   ],
   imports: [
     BrowserModule
