b28abfa944ea3ae35571ffeb8f4668f3bd2fcdda
mehedi-seu <mdmehedihasanroni28@gmail.com>
2020-02-09 01:08:16 +0600

    initial structure done

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,6 +7,8 @@ import {FooterComponent} from './main/footer/footer.component';
 import {AboutComponent} from './main/about/about.component';
 import {ProjectComponent} from './main/project/project.component';
 import {ResumeComponent} from './main/resume/resume.component';
+import { ContactComponent } from './main/contact/contact.component';
+import { MyServicesComponent } from './main/my-services/my-services.component';
 
 @NgModule({
     declarations: [
@@ -16,6 +18,9 @@ import {ResumeComponent} from './main/resume/resume.component';
         ResumeComponent,
         FooterComponent,
         ProjectComponent,
+        ContactComponent,
+        MyServicesComponent,
+        MyServicesComponent,
     ],
   imports: [
     BrowserModule
